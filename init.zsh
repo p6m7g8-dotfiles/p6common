f1992892ea420e5f62e16a18af92f7d7c36e4d70
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-18 11:36:30 -0400

    chore: converts to `__p6_dir` for portability (#147)

--- init.zsh
+++ init.zsh
@@ -1,17 +1,13 @@
-
 ######################################################################
 #<
 #
 # Function: p6df::modules::p6common::init()
 #
 #  Depends:	 p6_bootstrap
-#  Environment:	 P6_DFZ_SRC_DIR
 #>
 ######################################################################
 p6df::modules::p6common::init() {
 
-  local dir="$P6_DFZ_SRC_DIR/p6m7g8/p6common"
-
-  . $dir/lib/_bootstrap.sh
-  p6_bootstrap "$dir"
+  . $__p6_dir/lib/_bootstrap.sh
+  p6_bootstrap "$__p6_dir"
 }
