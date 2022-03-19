41850d28005f9e3a5e6cab8229014e83e067635d
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-19 12:18:51 -0400

    chore: chases dependencie renames

--- lib/stdlib/inc.sh
+++ lib/stdlib/inc.sh
@@ -33,10 +33,10 @@ p6_include_module() {
     local module="$1"
     local start="${2:-init.sh}"
 
-    . $P6_DFZ_SRC_P6M7G8_DIR/p6common/lib/io.sh
-    . $P6_DFZ_SRC_P6M7G8_DIR/p6common/lib/debug.sh
-    . $P6_DFZ_SRC_P6M7G8_DIR/p6common/lib/file.sh
-    . $P6_DFZ_SRC_P6M7G8_DIR/p6common/lib/dir.sh
+    . $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6common/lib/io.sh
+    . $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6common/lib/debug.sh
+    . $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6common/lib/file.sh
+    . $P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6common/lib/dir.sh
 
     p6_include__debug "module(): $module/$start"
     p6_file_load "$module/$start"
