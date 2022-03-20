192cac97a05a0c56a3e6a7f57145ee25e963a3c5
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-20 09:46:31 -0400

    chore: chases dependencie renames (#3)

--- lib/test/_bootstrap.sh
+++ lib/test/_bootstrap.sh
@@ -11,7 +11,7 @@
 #>
 ######################################################################
 p6_p6test_bootstrap() {
-  local dir="${1:-$P6_DFZ_SRC_P6M7G8_DIR/p6test}"
+  local dir="${1:-$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6test}"
 
   local file
   for file in $(find $dir -type f -name "*.sh" | xargs); do
