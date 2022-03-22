f20918ff85effecacb7d2f74ff96dde94a5075e9
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-03-22 10:29:06 -0400

    path change

--- bin/deps.sh
+++ bin/deps.sh
@@ -2,10 +2,10 @@
 
 if [ -n "$GITHUB_WORKFLOW" ]; then
     apk --no-cache add git    
-    git clone https://github.com/p6m7g8/p6ctl ../p6ctl
+    git clone https://github.com/p6m7g8-dotfiles/p6ctl ../p6ctl
 elif [ -n "$DOCKER_WORKFLOW" ]; then
     apk --no-cache add git    
-    git clone https://github.com/p6m7g8/p6ctl /p6m7g8/p6ctl
+    git clone https://github.com/p6m7g8-dotfiles/p6ctl /p6m7g8/p6ctl
 else
     # laptop in PATH already
     true
