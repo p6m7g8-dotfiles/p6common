eff168b4236d21d1e4f3cae4d17b7f2dac6aaf05
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-07-16 12:57:18 -0400

    chore: simplifies by avoiding chicken in egg

--- lib/_bootstrap.sh
+++ lib/_bootstrap.sh
@@ -12,12 +12,10 @@ p6_bootstrap() {
   local dir="${1:-$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6common}"
   local islocal="${2:-}"
 
-  p6_bootstrap_optimize "$dir" "$islocal"
-
   local t0=$EPOCHREALTIME
   local file
-  for file in $(p6_dir_list_recursive "$dir/lib"); do
-    p6_file_load "$file"
+  for file in $(find "$dir/lib" -type f); do
+    . "$file"
   done
   local t1=$EPOCHREALTIME
   p6_time "$t0" "$t1" "p6_dir_load($dir)"
@@ -25,41 +23,4 @@ p6_bootstrap() {
   p6_path_if "$dir/bin"
 
   p6_return_void
-}
-
-# this trims .3s
-######################################################################
-#<
-#
-# Function: p6_bootstrap_optimize(dir, islocal)
-#
-#  Args:
-#	dir -
-#	islocal -
-#
-#  Environment:	 XXX
-#>
-######################################################################
-p6_bootstrap_optimize() {
-  local dir="$1"
-  local islocal="$2"
-
-  if [ x"$islocal" != x"local" ]; then
-    # XXX: Chicken in Egg....
-    . $dir/../p6common/lib/stdlib/const.sh
-    . $dir/../p6common/lib/types/return.sh
-    . $dir/../p6common/lib/stdio/io.sh
-    . $dir/../p6common/lib/stdio/debug.sh
-    . $dir/../p6common/lib/string/string.sh
-    . $dir/../p6common/lib/stdio/file.sh
-    . $dir/../p6common/lib/stdio/dir.sh
-  elif [ x"islocal" = x"github" ]; then
-    . $dir/lib/stdlib/const.sh
-    . $dir/lib/types/return.sh
-    . $dir/lib/stdio/io.sh
-    . $dir/lib/stdio/debug.sh
-    . $dir/lib/string/string.sh
-    . $dir/lib/stdio/file.sh
-    . $dir/lib/stdio/dir.sh
-  fi
 }
