e47601a36b3a6db0f9939d0eee35a482b3cc5f41
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-04-14 16:04:00 -0400

    feat: +p6_string_eq_1(), p6_dt_seconds_to_hours(), p6_dt_relative_to_absolute(), p6_dt_delta_in_seconds()

--- lib/stdlib/dt.sh
+++ lib/stdlib/dt.sh
@@ -18,40 +18,34 @@ p6_dt__debug() {
 ######################################################################
 #<
 #
-# Function: str dt = p6_dt__date(fmt, offset)
+# Function: str dt = p6_dt__date(fmt, [offset=], [offset_fmt=])
 #
 #  Args:
 #	fmt -
-#	offset -
+#	OPTIONAL offset - []
+#	OPTIONAL offset_fmt - []
 #
 #  Returns:
 #	str - dt
 #
-#  Environment:	 BSD
 #>
 ######################################################################
 p6_dt__date() {
     local fmt="$1"
-    local offset="$2"
+    local offset="${2:-}"
+    local offset_fmt="${3:-}"
 
-    p6_dt__debug "_date(): date \"$fmt\" \"$offset\""
+    p6_dt__debug "_date(): date \"$fmt\" \"$offset\" \"$offset_fmt\""
 
     local dt
-    if [ -z "$offset" ]; then
-        dt=$(date $fmt)
+    if p6_string_blank "$offset"; then
+        dt=$(date "+$fmt")
     else
-        local os_name=$(p6_os_name)
-        case $os_name in
-        Darwin | BSD)
-            oft_flag="-v"
-            oft_val="${oft}d"
-            ;;
-        *)
-            oft_flag="--date"
-            oft_val="${oft} day ago"
-            ;;
-        esac
-        dt=$(date $fmt $oft)
+        if p6_string_blank "$offset_fmt"; then
+            dt=$(date -v "$offset" "+$fmt")
+        else
+            dt=$(date -j -f "$offset_fmt" "$offset" "+$fmt")
+        fi
     fi
 
     p6_return_str "$dt"
@@ -105,7 +99,7 @@ p6_dt_yesterday() {
 
     local fmt="+%Y%m%d"
 
-    local dt=$(p6_dt__date "$fmt" "-1")
+    local dt=$(p6_dt__date "$fmt" "-1d")
 
     p6_return_str "$dt"
 }
@@ -124,7 +118,7 @@ p6_dt_tomorrow() {
 
     local fmt="+%Y%m%d"
 
-    local dt=$(p6_dt__date "$fmt" "1")
+    local dt=$(p6_dt__date "$fmt" "1d")
 
     p6_return_str "$dt"
 }
@@ -148,4 +142,76 @@ p6_dt_mtime() {
     local modified_epoch_seconds=$(stat -f "%m" $file)
 
     p6_return_size_t "$modified_epoch_seconds"
+}
+
+######################################################################
+#<
+#
+# Function: int delta = p6_dt_delta_in_seconds(d1, d2, fmt)
+#
+#  Args:
+#	d1 -
+#	d2 -
+#	fmt -
+#
+#  Returns:
+#	int - delta
+#
+#>
+######################################################################
+p6_dt_delta_in_seconds() {
+    local d1="$1"
+    local d2="$2"
+    local fmt="$3"
+
+    local d1s=$(p6_dt__date "%s" "${d1}" "$fmt")
+    local d2s=$(p6_dt__date "%s" "${d2}" "$fmt")
+
+    local delta=$(p6_math_sub "$d2s" "$d1s")
+
+    p6_return_int "$delta"
+}
+
+######################################################################
+#<
+#
+# Function: p6_dt_seconds_to_hours(seconds)
+#
+#  Args:
+#	seconds -
+#
+#>
+######################################################################
+p6_dt_seconds_to_hours() {
+    local seconds="$1"
+
+    local hours=$(p6_echo "scale=2; $seconds / 3600" | bc -lq)
+
+    p6_return_float "$hours"
+}
+
+######################################################################
+#<
+#
+# Function: str absolute = p6_dt_relative_to_absolute(relative)
+#
+#  Args:
+#	relative -
+#
+#  Returns:
+#	str - absolute
+#
+#>
+######################################################################
+p6_dt_relative_to_absolute() {
+    local relative="$1"
+
+    local number=$(p6_echo "$relative" | awk '{print $1}')
+    local unit=$(p6_echo "$relative" | awk '{print $2}')
+
+    local unit_abbr=$(p6_echo "$unit" | cut -c 1)
+
+    local absolute=$(p6_dt__date "%Y-%m-%d" "-${number}${unit_abbr}")
+
+    p6_return_str "$absolute"
 }
