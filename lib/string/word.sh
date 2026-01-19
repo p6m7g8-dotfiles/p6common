8dea54f43a89de8fdd3a7870ea7af0980f742499
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-18 21:58:05 -0500

    feat: p6_word_unique -- maintain order (#55)

--- lib/string/word.sh
+++ lib/string/word.sh
@@ -28,18 +28,19 @@ p6_word__debug() {
 #
 #>
 ######################################################################
-p6_word_unique() {
-  shift 0
+p6_word_unique () {
+    shift 0
 
-  local pos
-  local words
-  words=$(for pos in "$@"; do
-    for i in $(echo $pos); do
-      echo "$i"
-    done
-  done | sort -u)
+    local words
+    words="$(
+        for pos in "$@"; do
+            for i in $pos; do
+                printf '%s\n' "$i"
+            done
+        done | awk '!seen[$0]++'
+    )"
 
-  p6_return_words "$words"
+    p6_return_words "$words"
 }
 
 ######################################################################
